1081cb94492656c1973a755408f0a877f0194529
junhee-ko <junheee.ko@gmail.com>
2021-07-05 16:20:13 +0900

    first commit

---
+++ src/main/kotlin/me/jko/restfulwebservicewithkotlin/RestfulWebServiceWithKotlinApplication.kt
@@ -0,0 +1,11 @@
+package me.jko.restfulwebservicewithkotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RestfulWebServiceWithKotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<RestfulWebServiceWithKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/me/jko/restfulwebservicewithkotlin/RestfulWebServiceWithKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.jko.restfulwebservicewithkotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RestfulWebServiceWithKotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
