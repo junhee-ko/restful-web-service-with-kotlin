ae19dbd67b27e45889bd9f97e1d612437d6d552c
junhee-ko <junheee.ko@gmail.com>
2021-07-05 16:37:13 +0900

    Add MessageService

---
+++ src/main/kotlin/me/jko/restfulwebservicewithkotlin/MessageService.kt
@@ -0,0 +1,13 @@
+package me.jko.restfulwebservicewithkotlin
+
+import org.springframework.stereotype.Service
+
+@Service
+class MessageService(val db: MessageRepository) {
+
+    fun findMessages(): List<Message> = db.findMessages()
+
+    fun post(message: Message){
+        db.save(message)
+    }
+}
