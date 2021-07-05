502cd8b5294b909e16800dc5d6358b050451b5c8
junhee-ko <junheee.ko@gmail.com>
2021-07-05 16:38:30 +0900

    Update MessageResource class to use MessageService

--- src/main/kotlin/me/jko/restfulwebservicewithkotlin/Application.kt
+++ src/main/kotlin/me/jko/restfulwebservicewithkotlin/Application.kt
@@ -5,6 +5,8 @@ import org.springframework.boot.runApplication
 import org.springframework.data.annotation.Id
 import org.springframework.data.relational.core.mapping.Table
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
@@ -15,13 +17,14 @@ fun main(args: Array<String>) {
 }
 
 @RestController
-class MessageResource {
+class MessageResource(val service: MessageService) {
     @GetMapping
-    fun index(): List<Message> = listOf(
-        Message("1", "Hello!"),
-        Message("2", "Bonjour!"),
-        Message("3", "Privet!"),
-    )
+    fun index(): List<Message> = service.findMessages()
+
+    @PostMapping
+    fun post(@RequestBody message: Message) {
+        service.post(message)
+    }
 }
 
 @Table("MESSAGES")
